25e765ca71b24ac94a7c6e63eaf9ef1632ca8568
YellowRegina <ccagtmst@outlook.com>
2026-06-14 19:48:55 +0800

    feat: register initial knife in CardPool on game start

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -4,7 +4,9 @@ extends Node
 const KNIFE_SCENE = preload("res://scenes/weapons/knife/knife_weapon.tscn")
 
 func _ready() -> void:
+	var gm = get_node("/root/GameManager")
 	var player := $YSort/Player as Player
-	player.leveled_up.connect(func(_lvl: int): GameManager.trigger_level_up())
-	player.died.connect(GameManager.game_over)
+	player.leveled_up.connect(func(_lvl: int): gm.trigger_level_up())
+	player.died.connect(gm.game_over)
 	player.add_weapon(KNIFE_SCENE)
+	CardPool.register_weapon(player, "knife")
