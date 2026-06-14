3780a900edb1825267ffc52e86e1aaade16aa12c
YellowRegina <ccagtmst@outlook.com>
2026-06-14 12:58:31 +0800

    feat: orb weapon with hit cooldown

---
+++ scenes/weapons/orb/orb_shield.gd
@@ -0,0 +1,38 @@
+# scenes/weapons/orb/orb_shield.gd
+extends Node2D
+
+const DAMAGE: float = 8.0
+const ORBIT_RADIUS: float = 60.0
+const ORBIT_SPEED: float = 2.0
+const HIT_COOLDOWN: float = 0.5
+const ORB_RADIUS: float = 14.0
+
+var orbit_index: int = 0
+var total_orbs: int = 2
+var _player: Node2D = null
+var _hit_cooldowns: Dictionary = {}
+
+func _ready() -> void:
+	_player = get_parent()
+
+func _process(delta: float) -> void:
+	if _player == null:
+		return
+	var angle := (TAU / total_orbs) * orbit_index + Time.get_ticks_msec() * 0.001 * ORBIT_SPEED
+	global_position = _player.global_position + Vector2(cos(angle), sin(angle)) * ORBIT_RADIUS
+	_check_hits()
+	_tick_cooldowns(delta)
+
+func _check_hits() -> void:
+	for enemy in get_tree().get_nodes_in_group("enemies"):
+		if enemy in _hit_cooldowns:
+			continue
+		if global_position.distance_to((enemy as Node2D).global_position) <= ORB_RADIUS:
+			(enemy as Enemy).take_damage(DAMAGE)
+			_hit_cooldowns[enemy] = HIT_COOLDOWN
+
+func _tick_cooldowns(delta: float) -> void:
+	for key in _hit_cooldowns.keys():
+		_hit_cooldowns[key] -= delta
+		if _hit_cooldowns[key] <= 0.0:
+			_hit_cooldowns.erase(key)
---
+++ scenes/weapons/orb/orb_weapon.gd
@@ -0,0 +1,17 @@
+# scenes/weapons/orb/orb_weapon.gd
+class_name OrbWeapon
+extends WeaponBase
+
+const ORB_SCENE = preload("res://scenes/weapons/orb/orb_shield.tscn")
+const NUM_ORBS: int = 2
+
+func _ready() -> void:
+	cooldown = 9999.0
+	for i in range(NUM_ORBS):
+		var orb := ORB_SCENE.instantiate()
+		get_parent().add_child(orb)
+		orb.orbit_index = i
+		orb.total_orbs = NUM_ORBS
+
+func attack() -> void:
+	pass
